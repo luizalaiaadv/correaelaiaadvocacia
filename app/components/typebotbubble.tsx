10fe09c72e6a3f91bcea833cca8fe0d76a9c66f9
maxinelobato <limabarreto.rg@gmail.com>
2026-04-16 17:49:01 -0300

    changes

--- app/components/typebotbubble.tsx
+++ app/components/typebotbubble.tsx
@@ -4,11 +4,13 @@ import { Bubble } from '@typebot.io/react';
 
 export default function TypebotBubble() {
   return (
-    <Bubble
-      typebot="correia-e-laia-lp-trabalhista-a-b-kb9c7vg"
-      apiHost="https://typebot.io"
-      previewMessage={{ message: 'Faça sua consultoria jurídica 👇' }}
-      theme={{ button: { backgroundColor: '#26d466', size: 'large' } }}
-    />
+    <>
+      <Bubble
+        typebot="correia-e-laia-lp-trabalhista-a-b-kb9c7vg"
+        apiHost="https://typebot.io"
+        previewMessage={{ message: 'Faça sua consultoria jurídica 👇' }}
+        theme={{ button: { backgroundColor: '#26d466', size: 'large' } }}
+      />
+    </>
   );
 }
