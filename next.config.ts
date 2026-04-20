3c41660ba166095788f9d21933837a82b62aef69
maxinelobato <limabarreto.rg@gmail.com>
2026-04-20 15:09:15 -0300

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from 'next';
+import ImageMinimizerPlugin from 'image-minimizer-webpack-plugin';
 
 const config: NextConfig = {
   images: {
@@ -11,6 +12,27 @@ const config: NextConfig = {
     ],
     formats: ['image/avif', 'image/webp'],
     qualities: [75, 85],
+    minimumCacheTTL: 31536000,
+  },
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.optimization.minimizer.push(
+        new ImageMinimizerPlugin({
+          minimizer: {
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            implementation: ImageMinimizerPlugin.imageminMinify as any,
+            options: {
+              plugins: [
+                ['imagemin-mozjpeg', { quality: 80, progressive: true }],
+                ['imagemin-pngquant', { quality: [0.65, 0.8] }],
+                ['imagemin-webp', { quality: 80 }],
+              ],
+            },
+          },
+        }),
+      );
+    }
+    return config;
   },
   experimental: {
     optimizePackageImports: ['motion', 'lucide-react', '@typebot.io/react'],
